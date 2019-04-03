a089d699af926fd6a2da934d03a06d399c73b02f
Adrian Rodriguez <adrian.rodriguez@mojix.com>
2019-04-03 14:38:03 -0400

    Added Activities

--- app/src/main/java/com/programacion3/progra3ejemplos/MarginPaddingActivity.java
+++ app/src/main/java/com/programacion3/progra3ejemplos/MarginPaddingActivity.java
@@ -3,11 +3,11 @@ package com.programacion3.progra3ejemplos;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MarginPaddingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_margin_padding);
     }
 }
