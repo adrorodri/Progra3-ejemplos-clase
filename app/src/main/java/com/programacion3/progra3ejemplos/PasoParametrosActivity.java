34072127b1c98946816d080923fb0bfd906c492e
Adrian Rodriguez <adrian.rodriguez@mojix.com>
2019-04-09 10:03:48 -0400

    Added PasoParametros Activities

---
+++ app/src/main/java/com/programacion3/progra3ejemplos/PasoParametrosActivity.java
@@ -0,0 +1,63 @@
+package com.programacion3.progra3ejemplos;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+public class PasoParametrosActivity extends AppCompatActivity {
+
+    EditText editTextParametro1;
+    EditText editTextParametro2;
+    CheckBox checkBoxParametro3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paso_parametros);
+
+        // Obtenemos la referencia de nuestros widgets del layout
+        editTextParametro1 = findViewById(R.id.editTextParametro1);
+        editTextParametro2 = findViewById(R.id.editTextParametro2);
+        checkBoxParametro3 = findViewById(R.id.checkBoxParametro3);
+    }
+
+    public void pasarParametros(View view) {
+        // Creamos un intent para pasar parametros
+        Intent intent = new Intent(this, RecibirParametrosActivity.class);
+
+        // Podemos pasar parametros con tipos de dato primitivos
+        String parametro1 = editTextParametro1.getText().toString();
+        String parametro2 = editTextParametro2.getText().toString();
+        boolean parametro3 = checkBoxParametro3.isChecked();
+        intent.putExtra("parametro1", parametro1);
+        intent.putExtra("parametro2", parametro2);
+        intent.putExtra("parametro3", parametro3);
+
+        // Tambien podemos pasar objetos, siempre y cuando su clase implemente a Serializable
+        EstudianteUPB estudianteUPB = new EstudianteUPB("Juan", "Perez", 12345);
+        intent.putExtra("estudiante", estudianteUPB);
+
+        // Iniciamos un nuevo activity y esperamos un resultado de este (con el codigo de referencia 123)
+        startActivityForResult(intent, 123);
+    }
+
+
+    // Esta funcion se llama automaticamente cuando el siguiente activity (que fue iniciado con startActivityForResult) se destruye
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        // Verificamos el requestCode coincida con el requestCode
+        switch (requestCode) {
+            case 123: {
+                if (resultCode == RESULT_OK) { // Si el resultado indica que todo esta OK, actualizamos el UI
+                    String parametro1 = data.getStringExtra("parametro1");
+                    editTextParametro1.setText(parametro1);
+                }
+                break;
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/programacion3/progra3ejemplos/RecibirParametrosActivity.java
@@ -0,0 +1,47 @@
+package com.programacion3.progra3ejemplos;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class RecibirParametrosActivity extends AppCompatActivity {
+
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recibir_parametros);
+
+        // Obtenemos los valores que nos paso el Intent que abrió este activity
+        Intent intent = getIntent();
+        String parametro1 = intent.getStringExtra("parametro1");
+        String parametro2 = intent.getStringExtra("parametro2");
+        boolean parametro3 = intent.getBooleanExtra("parametro3", false);
+
+        // Podemos obtener el objeto estudiante que es serializable
+        EstudianteUPB estudianteUPB = (EstudianteUPB) intent.getSerializableExtra("estudiante");
+
+        // Actualizamos el UI
+        TextView textView = findViewById(R.id.textViewParametros);
+        textView.setText(parametro1 + "\n" + parametro2 + "\n" + String.valueOf(parametro3));
+
+        editText = findViewById(R.id.editTextEditarParametro1);
+        editText.setText(parametro1);
+
+        Toast.makeText(this, estudianteUPB.getNombre(), Toast.LENGTH_LONG).show();
+    }
+
+    // Podemos sobreescribir la funcionalidad del botón de back
+    @Override
+    public void onBackPressed() {
+        // Creamos un Intent de respuesta
+        Intent intent = new Intent();
+        intent.putExtra("parametro1", editText.getText().toString());
+        setResult(RESULT_OK, intent); // Indicamos un resultCode = OK (Tenemos otras opciones RESULT_OK, RESULT_CANCELLED, RESULT_FIRST_USER)
+        finish(); // Finalizamos o destruimos manualmente nuestro activity. Este ya retornará el intent de resultado
+    }
+}
